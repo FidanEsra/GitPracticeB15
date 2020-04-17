1fb65093fb99c5b405bb5730d7a1472d46d4caf9
FidanEsra <esrasgt@hotmail.com>
2020-04-17 14:50:21 -0400

    My second commit

--- src/test/java/GitPractice.java
+++ src/test/java/GitPractice.java
@@ -7,5 +7,8 @@ public class GitPractice {
         System.out.println("Hello World");
         System.out.println("Hello World");
         //--------------------------------
+
+        System.out.println("Hello World");
+        System.out.println("Hello World");
     }
 }
